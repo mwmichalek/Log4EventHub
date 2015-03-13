d3c13ef9cc035a0aea5cd0b7c8c5d79d97a7ad43
mwmichalek <mwmichalek@gmail.com>
2015-03-13 17:02:51 -0400

    Initial creation

---
+++ Log4EventHub/Log4EventHubAppender.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Appender;
+using log4net.Core;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+
+namespace Log4EventHub
+{
+    public class Log4EventHubAppender : AppenderSkeleton {
+
+        private LoggingEventSerializer _serializer;
+
+        private EventHubClient _eventHubClient;
+
+        public string ApplicationName { get; set; }
+
+        public string ConnectionString { get; set; }
+
+        public string EventHubName { get; set; }
+
+        static Log4EventHubAppender() 
+        {
+            
+        }
+
+
+        public override void ActivateOptions() 
+        {
+            _eventHubClient = EventHubClient.CreateFromConnectionString(ConnectionString, EventHubName);
+            _serializer = new LoggingEventSerializer {
+                ApplicationName = ApplicationName
+            };
+        }
+
+        protected override void Append(LoggingEvent loggingEvent) 
+        {
+            var content = _serializer.SerializeLoggingEvents(new[] { loggingEvent });
+
+            var eventData = new EventData(Encoding.UTF8.GetBytes(content)) {
+                PartitionKey = "0"
+            };
+
+            _eventHubClient.SendAsync(eventData);
+        }
+    }
+}
---
+++ Log4EventHubTest/LoggerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using log4net.Repository.Hierarchy;
+using Microsoft.ServiceBus;
+using Xunit;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+
+namespace Log4EventHubTest
+{
+    public class LoggerTests {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof (LoggerTests));
+
+        public LoggerTests() {
+            var config = new FileInfo("log4net.config");
+            XmlConfigurator.Configure(config);
+        }
+
+        [Fact]
+        public void LoadLogger()
+        {
+            foreach (var number in Enumerable.Range(0, 100)) {
+                Logger.InfoFormat("Holy crap, this worked {0}!", number);
+                Thread.Sleep(5000);
+            }
+            Assert.True(true);
+
+        }
+
+    }
+
+}
